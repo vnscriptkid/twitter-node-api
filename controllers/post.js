563b8b976e2960d3e7fc7416c3bcf915bfba03f8
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-04 00:17:46 +0700

    feat: persist post to db

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,20 @@
+const Post = require("../models/post");
 const validationHandler = require("../validations/validationHandler");
 
 exports.index = (req, res) => {
   return res.send({ hi: "thanh" });
 };
 
-exports.store = (req, res, next) => {
+exports.store = async (req, res, next) => {
   try {
     validationHandler(req);
+
+    const post = new Post({
+      description: req.body.name,
+      image: "http://image.com/cat.png",
+    });
+
+    await post.save();
 
     res.send({ name: req.body.name });
   } catch (error) {
