2a25693bae9046e78e6ecbb3d2a550385a25bb45
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-04 23:05:12 +0700

    feat: guard me endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth");
+const passportJwt = require("../middlewares/passportJwt")();
 const { isEmail, hasPassword, hasName } = require("../validations/validators");
 
 router.post("/login", authController.login);
 router.post("/signup", [isEmail, hasPassword, hasName], authController.signup);
-router.get("/me", authController.me);
+router.get("/me", passportJwt.authenticate(), authController.me);
 
 module.exports = router;
