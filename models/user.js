9a6dd6a4aaf2e9a2c4f3e086a11db300f41718e6
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-04 21:41:08 +0700

    feat: user model, hash password

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true, select: false },
+  name: { type: String, required: true },
+});
+
+UserSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(5);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
+
+UserSchema.methods.isValidPassword = async function (candidatePassword) {
+  const result = await bcrypt.compare(candidatePassword, this.password);
+  return result;
+};
+
+module.exports = mongoose.model("user", UserSchema);
