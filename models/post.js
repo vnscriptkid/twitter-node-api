3d1ea416b0d0512e7785ee9d6bd89d7543a2ed32
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-04 21:42:28 +0700

    feat: connect post and user

--- models/post.js
+++ models/post.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   image: { type: String, required: true },
   description: { type: String, required: true },
+  user: { type: Schema.Types.ObjectId, ref: "user" },
   createdAt: { type: Date, default: Date.now() },
 });
 
