c4884ed96ffc5af414641c55ec2c31b277cbad22
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-06 22:10:26 +0700

    feat(post): update model fields

--- models/post.js
+++ models/post.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  image: { type: String, required: true },
-  description: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "user" },
-  createdAt: { type: Date, default: Date.now() },
-});
+const PostSchema = new Schema(
+  {
+    content: { type: String, trim: true },
+    postedBy: { type: Schema.Types.ObjectId, ref: "user" },
+    pinned: Boolean,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("post", PostSchema);
