4479adfcd9ccaa1c2716b32f0bff4697b703f461
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-06 21:03:53 +0700

    feat(user): add new user fields to model

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true, select: false },
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   following: [
     {
       type: Schema.Types.ObjectId,
