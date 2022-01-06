ac744d38c7a49176eee492da471aab0b61e81f74
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-06 21:34:14 +0700

    feat(user): attach updatedAt, createdAt

--- models/user.js
+++ models/user.js
@@ -3,20 +3,25 @@ const bcrypt = require("bcryptjs");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true, select: false },
-  firstName: { type: String, required: true, trim: true },
-  lastName: { type: String, required: true, trim: true },
-  profilePic: { type: String, default: "/images/profilePic.png" },
-  following: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  ],
-});
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, select: false },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    profilePic: { type: String, default: "/images/profilePic.png" },
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 UserSchema.methods.encryptPassword = async (password) => {
   const salt = await bcrypt.genSalt(5);
