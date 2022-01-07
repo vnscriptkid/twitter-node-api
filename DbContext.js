9cde95bc59703751592a471dee6a4d09c8d07e71
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-07 20:45:41 +0700

    chore(db): do not init connection if existed

--- DbContext.js
+++ DbContext.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 const config = require("./config");
 
 class DbContext {
-  static async connect() {
+  static connection = null;
+
+  static async _connect() {
     return mongoose
       .connect(config.dbUrl)
       .then(() => console.log("db connected"))
       .catch((err) => console.log("can not connect to db: ", err));
+  }
+
+  static async connect() {
+    if (!DbContext.connection) {
+      await DbContext._connect();
+    }
+
+    return DbContext.connection;
   }
 }
 
