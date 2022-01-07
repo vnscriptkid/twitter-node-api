ef8d6e60092481dff9948879b19a7e002c56c2e7
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-07 21:04:24 +0700

    chore: build user test util

--- tests/utils/db-utils.js
+++ tests/utils/db-utils.js
@@ -1,5 +1,7 @@
+const faker = require("faker");
 const Post = require("../../models/Post");
 const User = require("../../models/User");
+const { hashPassword } = require("../../utils/auth");
 
 exports.resetDb = async () => {
   await User.deleteMany({});
@@ -9,3 +11,15 @@ exports.resetDb = async () => {
 function insertUsers() {}
 
 function insertPosts() {}
+
+exports.buildUser = async () => {
+  const user = await User.create({
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    password: await hashPassword("123456"),
+  });
+
+  return user;
+};
