a89c68d29292286d7514560f40fac45f324013fc
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-07 18:52:11 +0700

    chore: separate startServer for testing

---
+++ startServer.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const rateLimit = require("express-rate-limit");
+
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const followRoutes = require("./routes/follow");
+const errorHandler = require("./middlewares/errorHandler");
+const passportJwt = require("./middlewares/passportJwt")();
+
+const startServer = ({ port = process.env.PORT } = {}) => {
+  const app = express();
+
+  const limiter = rateLimit({
+    windowMs: 15 * 1000, // 15 seconds
+    max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  });
+
+  app.use(limiter);
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, "public")));
+
+  app.use(passportJwt.initialize());
+
+  app.use("/api/posts", passportJwt.authenticate(), postRoutes);
+  app.use("/api/auth", authRoutes);
+  app.use("/api/follow", passportJwt.authenticate(), followRoutes);
+
+  app.use(errorHandler);
+
+  return new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      console.log(`Server is listening on port ${server.address().port}`);
+
+      const originalClose = server.close.bind(server);
+
+      server.close = () => {
+        return new Promise((resolveClose) => {
+          originalClose(resolveClose);
+        });
+      };
+
+      resolve(server);
+    });
+  });
+};
+
+module.exports = startServer;
