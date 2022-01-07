df7927b9aec555829e43d4fbfc1f4309fbee23c0
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-07 20:59:07 +0700

    refactor: hash password auth util

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,11 @@
 const jwt = require("jwt-simple");
 const config = require("../config");
+const bcrypt = require("bcryptjs");
 
 exports.getUserToken = (id) => jwt.encode({ id }, config.jwtSecret);
+
+exports.hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(5);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
