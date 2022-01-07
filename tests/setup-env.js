e71607e88e210b117c074c25ea5906ec5cf33753
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-07 19:57:00 +0700

    chore(test): setup jest

---
+++ tests/setup-env.js
@@ -0,0 +1,5 @@
+process.env.PORT = 0;
+process.env.JWT_SECRET = "abdfafwoeij";
+process.env.REDIS_HOST = "localhost";
+process.env.REDIS_PORT = 6375;
+process.env.DB_URL = "mongodb://localhost:27015/twitter-test";
