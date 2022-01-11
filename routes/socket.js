3f773d7f21f1d1c46bcd519df30a76b9d42e84a9
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-12 02:09:21 +0700

    feat(socket): notify others in chat about new msg

--- routes/socket.js
+++ routes/socket.js
@@ -2,10 +2,24 @@ module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("connected to socket io");
 
+    socket.on("setup", (userId) => socket.join(userId));
+
     socket.on("join chat", (chatId) => socket.join(chatId));
 
     socket.on("typing", (chatId) => socket.in(chatId).emit("typing"));
 
     socket.on("stop typing", (chatId) => socket.in(chatId).emit("stop typing"));
+
+    socket.on("new message", (message) => {
+      const { sender, content, chat: { users } = {} } = message;
+
+      if (!users) return;
+
+      users.forEach(({ _id }) => {
+        if (!_id) return;
+
+        socket.in(_id).emit("new message", message);
+      });
+    });
   });
 };
