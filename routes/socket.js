c317444fc375a2a2950cc29c77bafaf7b6581a75
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-12 01:16:41 +0700

    feat(socket): join chat, notify typing

--- routes/socket.js
+++ routes/socket.js
@@ -1,5 +1,9 @@
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("connected to socket io");
+
+    socket.on("join chat", (chatId) => socket.join(chatId));
+
+    socket.on("typing", (chatId) => socket.in(chatId).emit("typing"));
   });
 };
