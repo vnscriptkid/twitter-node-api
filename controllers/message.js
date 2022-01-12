7638af751f22974b9af7f4fea3163efd3193ab6a
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-12 21:57:35 +0700

    feat(notif): newMessage notifs for users in chat except sender

--- controllers/message.js
+++ controllers/message.js
@@ -2,6 +2,7 @@ const { matchedData } = require("express-validator");
 const Chat = require("../models/Chat");
 const Message = require("../models/Message");
 const validationHandler = require("../validations/validationHandler");
+const Notification = require("../models/Notification");
 
 exports.store = async (req, res, next) => {
   try {
@@ -18,12 +19,36 @@ exports.store = async (req, res, next) => {
     message = await message.populate(["sender", "chat"]);
 
     // save newly created message as latest in chat group
-    await Chat.findByIdAndUpdate(chatId, {
-      latestMessage: message,
-    });
+    const chat = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        latestMessage: message,
+      },
+      { new: true }
+    );
+
+    // create notifications for all users in chat except sender
+    await createNotifsForNewMessage({ chat, message });
 
     return res.send(message);
   } catch (err) {
     next(err);
   }
 };
+
+function createNotifsForNewMessage({ chat, message }) {
+  return Promise.all(
+    chat.users
+      .map((userId) => {
+        if (userId === message.sender.id) return null;
+
+        return Notification.insertNotification({
+          userTo: userId,
+          userFrom: message.sender.id,
+          notificationType: "newMessage",
+          entityId: message._id,
+        });
+      })
+      .filter((x) => !!x)
+  );
+}
